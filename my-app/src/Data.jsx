8e34137d64ccc8f1213492df2b77b73cfd92ab7b
vighneshnayak123 <116252558+vighneshnayak123@users.noreply.github.com>
2022-10-20 14:28:20 +0530

    Add files via upload

---
+++ my-app/src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        id : 0,
+        title : "Ray-ban",
+        price : 17,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass1.png"
+    },
+    {
+        id : 1,
+        title : "John Jacobs",
+        price : 14,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass2.png"
+    },
+    {
+        id : 2,
+        title : "Vincent Chase",
+        price : 21,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass3.png"
+    },
+    {
+        id : 3,
+        title : "Elegante",
+        price : 10,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass4.png"
+    },
+    {
+        id : 4,
+        title : "Fastrack",
+        price : 11,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass5.png"
+    },
+    {
+        id : 5,
+        title : "Black Jones",
+        price : 19,
+        desc : "Sunglasses or sun glasses (informally called shades or sunnies; more names below) are a form of protective eyewear designed primarily to prevent bright sunlight and high-energy visible light from damaging or discomforting the eyes.",
+        img : "/assets/images/products/sunglass6.jpeg"
+    }
+]
+
+export default DATA;
